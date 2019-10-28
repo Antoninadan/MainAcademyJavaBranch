d5e33950dd8a78597ca6642eca610e754218f51b
Antoninadan <mail100@i.ua>
2019-10-28 08:06:33 +0200

    change master 2

--- src/com/mainacad/service/OneService.java
+++ src/com/mainacad/service/OneService.java
@@ -2,12 +2,12 @@ package com.mainacad.service;
 
 public class OneService {
     public static String methodOne(){
-        String str = "methodOne: Change 1 - master";
+        String str = "methodOne: Change 1 - master 2";
         return str;
     }
 
     public static String methodTwo(){
-        String str = "methodTwo: Change 1 - master";
+        String str = "methodTwo: Change 1 - master 2";
         return str;
     }
 }
